26195cba6b0c4678f674fec54226d550e7657854
jorgesece <jorgesece@lip.pt>
2016-10-17 17:04:15 +0100

    add create ssl request

--- ooi/tests/integration/keystone/session.py
+++ ooi/tests/integration/keystone/session.py
@@ -26,7 +26,7 @@ IP_SERVER="192.92.149.119"
 
 class KeySession(object):
 
-    def __init__(self, auth_url = 'http://192.92.149.119:5000/v2.0'):
+    def __init__(self, auth_url = 'https://192.92.149.119:5000/v2.0'):
         self.auth_url = auth_url
 
     def create_session(self, user, password, project):
@@ -43,7 +43,8 @@ class KeySession(object):
         keystone = client.Client(auth_url=self.auth_url,
                                  username=user,
                                  password=password,
-                                 project_id=project)
+                                 project_id=project,
+                                 insecure=True)
 
         return keystone.auth_ref # token in keystone.auth_ref.auth_token
 
@@ -69,6 +70,15 @@ class KeySession(object):
         kwargs["http_version"] = "HTTP/1.1"
         kwargs["server_name"] = IP_SERVER
         kwargs["server_port"] = "9696"
+        return webob.Request.blank(path=path, environ=environ, base_url=base_url, headers=headers, **kwargs)
+
+    def create_request_ssl(self, app, path="/", environ={}, headers=None, base_url="/v2.0", **kwargs):
+        environ ["HTTP_X-Auth-Token"]= app.auth_token #"4cf9e807516c450fa98f320f4a9f431a
+        kwargs["http_version"] = "HTTP/1.1"
+        kwargs["server_name"] = IP_SERVER
+        kwargs["server_port"] = "9696"
+        # urlparse.urlsplit(base_url)
+        environ['wsgi.url_scheme'] = "https"
 
         return webob.Request.blank(path=path, environ=environ, base_url=base_url, headers=headers, **kwargs)
 
