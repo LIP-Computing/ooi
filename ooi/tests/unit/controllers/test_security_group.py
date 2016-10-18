0d8cadc49c87c345f9253cb2ab466f4e03e6457b
jorgesece <jorgesece@lip.pt>
2016-10-18 17:18:32 +0100

    add test wrong rules parameter

--- ooi/tests/unit/controllers/test_security_group.py
+++ ooi/tests/unit/controllers/test_security_group.py
@@ -115,8 +115,7 @@ class TestSecurityGroupControllerNeutron(base.TestController):
         self.assertIsInstance(ret.resources[0], occi_security_group.SecurityGroup)
         self.assertEqual(expected[0], ret.resources[0])
 
-    @mock.patch.object(helpers_neutron.OpenStackNeutron, "create_security_group")
-    def test_create_error(self, m):
+    def test_create_error(self):
         test_networks = fakes.networks[fakes.tenants["foo"]["id"]]
         schema1 = occi_security_group.SecurityGroup.kind.scheme
         net = test_networks[0]
@@ -127,128 +126,14 @@ class TestSecurityGroupControllerNeutron(base.TestController):
 
         self.assertRaises(exception.Invalid, self.controller.create, req)
 
-
-class TestSecurityGroupControllerNova(base.TestController):
-
-    def setUp(self):
-        super(TestSecurityGroupControllerNova, self).setUp()
-        # self.controller = network_api.Controller(None)
-
-    # @mock.patch.object(helpers.OpenStackHelper, "list_networks")
-    # def test_index(self, m_index):
-    #     test_networks = [
-    #         fakes.networks[fakes.tenants["bar"]["id"]],
-    #         fakes.networks[fakes.tenants["foo"]["id"]]
-    #     ]
-    #     req = fakes.create_req_test(None, None)
-    #     for nets in test_networks:
-    #         ooi_net = helpers.OpenStackHelper._build_networks(nets)
-    #         m_index.return_value = ooi_net
-    #         result = self.controller.index(req)
-    #         expected = self.controller._get_network_resources(ooi_net)
-    #         self.assertEqual(result.resources.__len__(),
-    #                          expected.__len__())
-    #         m_index.assert_called_with(req)
-    #
-    # @mock.patch.object(helpers.OpenStackHelper, "get_network_details")
-    # def test_show(self, m_network):
-    #     test_networks = fakes.networks[fakes.tenants["foo"]["id"]]
-    #     for net in test_networks:
-    #         ret = self.controller.show(None, net["id"])
-    #         self.assertIsInstance(ret, occi_network.NetworkResource)
-    #
-    # @mock.patch.object(helpers.OpenStackHelper, "create_network")
-    # def test_create(self, m):
-    #     test_networks = fakes.networks[fakes.tenants["foo"]["id"]]
-    #     for test_net in test_networks:
-    #         parameters = {"occi.core.title": test_net["name"],
-    #                       "org.openstack.network.ip_version": 4,
-    #                       "occi.network.address": "0.0.0.0",
-    #                       }
-    #         categories = {occi_network.NetworkResource.kind,
-    #                       occi_network.ip_network}
-    #         req = fakes.create_req_test_occi(parameters, categories)
-    #         fake_net = fakes.fake_build_net(
-    #             parameters['occi.core.title'],
-    #             parameters['org.openstack.network.ip_version'],
-    #             parameters['occi.network.address']
-    #         )
-    #         m.return_value = fake_net
-    #         ret = self.controller.create(req)
-    #         net = ret.resources.pop()
-    #         self.assertIsInstance(net, occi_network.NetworkResource)
-    #         self.assertEqual(net.title, test_net['name'])
-    #
-    # @mock.patch.object(helpers.OpenStackHelper, "create_network")
-    # def test_create_error(self, m):
-    #     test_networks = fakes.networks[fakes.tenants["foo"]["id"]]
-    #     schema1 = occi_network.NetworkResource.kind.scheme
-    #     net = test_networks[0]
-    #     schemes = {schema1: net}
-    #     parameters = {"occi.core.title": "name",
-    #                   }
-    #     req = fakes.create_req_test(parameters, schemes)
-    #
-    #     self.assertRaises(exception.Invalid, self.controller.create, req)
-    #
-    # @mock.patch.object(helpers.OpenStackHelper, "create_network")
-    # def test_create_no_ip_mixin(self, m):
-    #     test_networks = fakes.networks[fakes.tenants["foo"]["id"]]
-    #     for test_net in test_networks:
-    #         parameters = {"occi.core.title": test_net["name"],
-    #                       "org.openstack.network.ip_version": 4,
-    #                       "occi.network.address": "0.0.0.0",
-    #                       }
-    #         categories = {occi_network.NetworkResource.kind}
-    #         req = fakes.create_req_test_occi(parameters, categories)
-    #         fake_net = fakes.fake_build_net(
-    #             parameters['occi.core.title'],
-    #             parameters['org.openstack.network.ip_version'],
-    #             parameters['occi.network.address']
-    #         )
-    #         m.return_value = fake_net
-    #         self.assertRaises(exception.OCCIMissingType,
-    #                           self.controller.create, req)
-    #
-    # @mock.patch.object(helpers.OpenStackHelper, "delete_network")
-    # def test_delete(self, m_network):
-    #     m_network.return_value = []
-    #     test_networks = fakes.networks[fakes.tenants["foo"]["id"]]
-    #     for net in test_networks:
-    #         ret = self.controller.delete(None, net["id"])
-    #         self.assertEqual(ret, [])
-    #         self.assertEqual(ret.__len__(), 0)
-    #
-    # def test_get_network_resources(self):
-    #     test_networks = fakes.networks[fakes.tenants["foo"]["id"]]
-    #     subnet = fakes.subnets
-    #     for net in test_networks:
-    #         net["subnet_info"] = subnet[0]
-    #     ooi_net = (
-    #         helpers_neutron.OpenStackNeutron._build_networks(
-    #             test_networks))
-    #     ret = self.controller._get_network_resources(ooi_net)
-    #     self.assertIsInstance(ret, list)
-    #     self.assertIsNot(ret.__len__(), 0)
-    #     for net_ret in ret:
-    #         self.assertIsInstance(net_ret, occi_network.NetworkResource)
-    #
-    # def test_run_action_invalid(self):
-    #     tenant = fakes.tenants["foo"]
-    #     req = self._build_req(tenant["id"], path="/network?action=start")
-    #     server_uuid = uuid.uuid4().hex
-    #     self.assertRaises(exception.InvalidAction,
-    #                       self.controller.run_action,
-    #                       req,
-    #                       server_uuid,
-    #                       None)
-    #
-    # def test_run_action_up(self):
-    #     tenant = fakes.tenants["foo"]
-    #     req = self._build_req(tenant["id"], path="/network?action=up")
-    #     server_uuid = uuid.uuid4().hex
-    #     self.assertRaises(exception.NotImplemented,
-    #                       self.controller.run_action,
-    #                       req,
-    #                       server_uuid,
-    #                       None)
+    def test_create_invalid_param_rule(self):
+        tenant_id = fakes.tenants["foo"]["id"]
+        sec_group = openstack_helper.build_security_group_from_neutron(
+            fakes.security_groups[tenant_id]
+        )[0]
+        params = {"occi.core.title": "group",
+                  "occi.securitygroup.rules": "{'wrong': 'value'}]"
+                 }
+        categories = {occi_security_group.SecurityGroup.kind}
+        req = fakes.create_req_test_occi(params, categories)
+        self.assertRaises(exception.Invalid, self.controller.create, req)
